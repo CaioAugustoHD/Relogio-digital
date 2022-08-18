ab8b110057a772616a90c15f7097e258b5adcd1f
CaioAugustoHD <caioaugustosbs@gmail.com>
2022-08-18 13:58:49 -0300

    Modificado alguns detalhes no CSS e adicionado arquivo JavaScript

---
+++ index.js
@@ -0,0 +1,23 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+setInterval(function relogio(){
+    let data = new Date();
+    let hr = data.getHours();
+    let min = data.getMinutes();
+    let s = data.getSeconds();
+    
+    if(hr < 10){
+        hr = '0' + hr;
+    }
+    if(min < 10){
+        min = '0' + min;
+    }
+    if(s < 10){
+        s = '0' + s
+    }
+    horas.innerHTML = hr;
+    minutos.innerHTML = min;
+    segundos.innerHTML = s;
+},500);
